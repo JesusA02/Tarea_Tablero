f6e50e37a16a93f29b7036d8a09211e42d3e59b4
JesusA02 <cortesj02@estudiantec.cr>
2022-02-23 16:15:45 -0600

    Add Correción  Nueva

--- Tablero.py
+++ Tablero.py
@@ -11,22 +11,48 @@ class Tablero:
     #y un mecanismo para saber quién sigue en el turno
 
 
-
-
     #agregue parámetros necesarios después de self
     #para iniciar los valores de sus atributos
     def __init__(self, casillas_, fichas ):
         self.casillas = casillas_
         self.fichas = fichas
+        tablero = Tablero(40, fichas)
+        
+        ficha1 = Ficha("roja")
+        fichas.append(ficha1)
+
+        ficha2 = Ficha("azul")
+        fichas.append(ficha2)
+        
+
+        ficha3 = Ficha("verde")
+        fichas.append(ficha3)
+        
         #inicialice aquí todos los atributos
         #no olvide usar self.atributo para acceder el atributo de la clase
+
+    def turnos(self):
+        contador = 0
+        avanzar = True
+        while avanzar:
+            if contador == 0:
+                self.fichas[0].avanzar()
+                contador +=1
+            elif contador == 1:
+                self.fichas[1].avanzar()
+                contador +=1
+            elif contador == 2:
+                self.fichas[2].avanzar()
+                contador +=1
+            elif contador == 3:
+                contador = 0
 
 
     #defina aquí los métodos de Tablero
     #recuerde que el primer parámetro de cada método es siempre self
 
-ficha1 = Ficha("azul")
-ficha2 = Ficha("roja")
-tablero = Tablero(40, [fichas])
-print("Tablero", tablero.fichas[0].color)
+#ficha = Ficha("azul")
+#ficha2 = Ficha("roja")
+#tablero = Tablero(40, [ficha])
+#print("Tablero", tablero.fichas[0].color)
 #print("casillas", tablero.casillas)
