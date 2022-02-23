3082abcba1bab893efa57cfb2b6a47707645b357
JesusA02 <cortesj02@estudiantec.cr>
2022-02-22 22:18:06 -0600

    Add Tablero

--- Tablero.py
+++ Tablero.py
@@ -5,6 +5,7 @@ class Tablero:
     #Defina aquí los atributos de Tablero
     
     casillas = 0
+    fichas = []
 
     #también va a necesitar una lista de Fichas (puede asumir un número de Fichas fijo si le parece más fácil), 
     #y un mecanismo para saber quién sigue en el turno
@@ -14,10 +15,18 @@ class Tablero:
 
     #agregue parámetros necesarios después de self
     #para iniciar los valores de sus atributos
-    #def __init__(self):
+    def __init__(self, casillas_, fichas ):
+        self.casillas = casillas_
+        self.fichas = fichas
         #inicialice aquí todos los atributos
         #no olvide usar self.atributo para acceder el atributo de la clase
 
 
     #defina aquí los métodos de Tablero
     #recuerde que el primer parámetro de cada método es siempre self
+
+ficha1 = Ficha("azul")
+ficha2 = Ficha("roja")
+tablero = Tablero(40, [fichas])
+print("Tablero", tablero.fichas[0].color)
+#print("casillas", tablero.casillas)
